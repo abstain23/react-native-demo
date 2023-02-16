5b66f3302f965b063c1a54d01059fa1a3311fa3e
cc <1442122744@qq.com>
2023-02-16 18:41:27 +0800

    feat: 增加启动页

--- android/app/src/main/java/com/rndemonew/MainActivity.java
+++ android/app/src/main/java/com/rndemonew/MainActivity.java
@@ -6,6 +6,8 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+import com.zoontek.rnbootsplash.RNBootSplash;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -36,6 +38,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this);
     super.onCreate(null);
   }
 }
--- pages/WorkersScreen.tsx
+++ pages/WorkersScreen.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 import WebView from 'react-native-webview';
 import {View, StatusBar} from 'react-native';
 
+import RNBootSplash from 'react-native-bootsplash';
+
 class WorkersScreen extends React.Component {
+  handleOnLoad = () => {
+    RNBootSplash.hide({fade: true, duration: 500});
+  };
+
   render() {
     return (
       <View style={{flex: 1}}>
@@ -16,6 +22,7 @@ class WorkersScreen extends React.Component {
           mixedContentMode={'always'}
           javaScriptEnabled={true}
           androidHardwareAccelerationDisabled={true}
+          onLoad={this.handleOnLoad}
         />
       </View>
     );
